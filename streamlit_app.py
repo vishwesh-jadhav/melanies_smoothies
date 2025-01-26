1fc24028c77c22d7c6001ca13a312249c6611d4c
vishwesh-jadhav <85167154+vishwesh-jadhav@users.noreply.github.com>
2025-01-26 20:44:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,4 @@ if ingredients_list:
         st.rerun()  # Refresh the app to show updated orders
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
